2d0a5541989e3f075ea49102e38dbdde2be805c0
aliencoder <aliencoder@126.com>
2018-05-23 19:40:26 +0800

    add http request

--- http.go
+++ http.go
@@ -5,37 +5,71 @@ import (
 	"net/http"
 	"strings"
 	"net/url"
+	"encoding/json"
 )
 
-func HttpGet(url string) ([]byte, error) {
+var UnSerialize func(bytes []byte, result interface{}) (interface{}, error)
+
+func HttpGet(url string, options ...interface{}) (interface{}, error) {
 	resp, err := http.Get(url)
+	defer resp.Body.Close()
+
 	if err != nil {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
+	return readAndUnSerialize(resp, options)
 }
 
-func HttpPost(url string, args string) ([]byte, error) {
+func HttpPost(url string, args string, options ...interface{}) (interface{}, error) {
 	resp, err := http.Post(url,
 		"application/x-www-form-urlencoded",
 		strings.NewReader(args))
+	defer resp.Body.Close()
+
 	if err != nil {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
+	return readAndUnSerialize(resp, options)
 }
 
-func HttpPostForm(url string, args url.Values) ([]byte, error) {
+func HttpPostForm(url string, args url.Values, options ...interface{}) (interface{}, error) {
 	resp, err := http.PostForm(url, args)
+	defer resp.Body.Close()
 
 	if err != nil {
 		return nil, err
 	}
 
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
+	return readAndUnSerialize(resp, options)
+}
+
+func readAndUnSerialize(resp *http.Response, options []interface{}) (interface{}, error) {
+	if bytes, err := ioutil.ReadAll(resp.Body); err == nil {
+		if UnSerialize == nil {
+			return bytes, nil
+		} else {
+			target := options[0]
+			return UnSerialize(bytes, target)
+		}
+	} else {
+		return nil, err
+	}
+}
+
+func HttpJsonSerializefunc(bytes []byte, result interface{}) (interface{}, error) {
+	var data interface{}
+
+	if result == nil{
+		data = make(map[string]interface{})
+	}else{
+		data = result
+	}
+
+	if err := json.Unmarshal(bytes, data); err == nil{
+		return data, nil
+	}else{
+		return nil, err
+	}
 }
